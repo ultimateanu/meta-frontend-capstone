a3d4e2440b83e65a2ba405e43c9a61ec15c4a5bc
Bandi, Anirudh <ultimateanu@gatech.edu>
2023-09-14 13:18:06 -0700

    using flexbox

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,12 +3,12 @@ function Nav() {
     return (
         <nav>
             <ul>
-                <li><a>Home</a></li>
-                <li><a>About</a></li>
-                <li><a>Menu</a></li>
-                <li><a>Reservations</a></li>
-                <li><a>Order Online</a></li>
-                <li><a>Login</a></li>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/reservations">Reservations</a></li>
+                <li><a href="/order">Order Online</a></li>
+                <li><a href="/login">Login</a></li>
             </ul>
         </nav>
     );
