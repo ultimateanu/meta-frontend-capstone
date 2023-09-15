41d14375c817a212797247a69d1fca915643d81a
Bandi, Anirudh <ultimateanu@gatech.edu>
2023-09-14 17:37:20 -0700

    adding label tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import BookingForm from './components/BookingForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("Booking form labels present", () => {
+  test('Renders date label for BookingForm', () => {
+    render(<BookingForm availableTimes={["5pm", "6pm"]} />);
+    const dateLabel = screen.getByText("Choose date");
+    expect(dateLabel).toBeInTheDocument();
+  });
+
+  test('Renders time label for BookingForm', () => {
+    render(<BookingForm availableTimes={["5pm", "6pm"]} />);
+    const timeLabel = screen.getByText("Choose time");
+    expect(timeLabel).toBeInTheDocument();
+  });
+
+  test('Renders guests label for BookingForm', () => {
+    render(<BookingForm availableTimes={["5pm", "6pm"]} />);
+    const label = screen.getByText("Number of guests");
+    expect(label).toBeInTheDocument();
+  });
+
+  test('Renders occasion label for BookingForm', () => {
+    render(<BookingForm availableTimes={["5pm", "6pm"]} />);
+    const label = screen.getByText("Occasion");
+    expect(label).toBeInTheDocument();
+  });
 });
--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,43 +1,4 @@
-import { useState } from "react";
-
-
-function BookingForm(props) {
-    const availableOccasions = ["-", "Birthday", "Anniversary"]
-
-    // State
-    const [chosenDate, setChosenDate] = useState((new Date()).toISOString().split('T')[0]);
-    const [chosenTime, setChosenTime] = useState(props.availableTimes[0]);
-    const [numGuests, setNumGuests] = useState(4);
-    const [occasion, setOccasion] = useState(availableOccasions[0]);
-
-    const handleDateChange = (e) => {
-        setChosenDate(e.target.value);
-        props.updateAvailableTimes(e.target.value);
-    };
-
-    return (
-        <form aria-label="Make a reservation" style={{ display: "grid", maxWidth: "200px", gap: "20px" }}>
-            <label htmlFor="res-date">Choose date</label>
-            <input value={chosenDate} onChange={handleDateChange}
-                type="date" id="res-date"></input>
-
-            <label htmlFor="res-time">Choose time</label>
-            <select value={chosenTime} onChange={e => setChosenTime(e.target.value)} id="res-time ">
-                {props.availableTimes.map(t => <option key={t}>{t}</option>)}
-            </select>
-
-            <label htmlFor="guests">Number of guests</label>
-            <input value={numGuests} onChange={e => setNumGuests(e.target.value)}
-                type="number" placeholder="1" min="1" max="10" id="guests"></input>
-
-            <label htmlFor="occasion">Occasion</label>
-            <select value={occasion} onChange={e => setOccasion(e.target.value)} id="occasion">
-                {availableOccasions.map(t => <option key={t}>{t}</option>)}
-            </select>
-            <input type="submit" value="Make Your reservation"></input>
-        </form>
-    );
-}
+import BookingForm from "./BookingForm";
 
 function BookingPage(props) {
     return (
